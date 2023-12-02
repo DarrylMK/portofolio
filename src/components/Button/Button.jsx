98c0537547f43125f1811d77468dfd35af999a82
Darryl <darryl.matthew.k@gmail.com>
2023-12-02 13:53:21 +0700

    Updates

--- src/components/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import "./Button.css";
-
-const Button = ({ text, color, handleClick }) => {
-    return <button onClick={handleClick} style={{ '--button-color': color }}>{text}</button>;
-  };  
-
-export default Button;
